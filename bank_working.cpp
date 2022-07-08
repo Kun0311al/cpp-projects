d082689452116c9758df202d0475dcc56859e7cf
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-08 11:03:16 +0530

    frame work for the bank assistant

--- bank_working.cpp
+++ bank_working.cpp
@@ -1,5 +1,84 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+class n_acc{
+    public:
+        n_acc(){
+            user = new char[20];
+            amount =new float;
+            number = new long;
+        }
+
+        void New_account();
+        void showdata();
+        int search(char[]);
+         
+    private:
+        char *user;
+        float *amount;
+        long  *number;
+};
+
+void n_acc::New_account(){
+    cin.ignore();
+    cout<<"Enter Your name: "<<endl;
+    cin.getline(user,20);
+    cout<<"Enter amount:"<<endl;
+    cin>>*amount;
+    cout<<"Enter phone no.: "<<endl;
+    cin>>*number;
+}
+
+void n_acc::showdata(){
+    
+}
+
 int main(){
+    n_acc* U[20];
+    int choice{};
+    int i{0};
+    char user_n[20];
+    while (true)
+    {
+        cout<<"1. New Account"<<endl;
+        cout<<"2. Balance enquiry"<<endl;
+        cout<<"3. Cash withdrawal"<<endl;
+        cout<<"4. Cash deposit"<<endl;
+        cout<<"5. Edit name"<<endl;
+        cin>>choice;
+        
+        switch (choice)
+        {
+        case 1:
+            U[i]=new n_acc;
+            U[i]->New_account();
+            i++;
+            break;
+        case 2:
+            cin.ignore();
+            cout<<"Enter user name: "<<endl;
+            cin.getline(user_n,20);
+            for(int t=0;t<i;t++){
+                if (U[t]->search(user))
+                {
+                    cout<<"User found succesfully";
+                    U[t]->showdata();
+                    break;
+                }
+                
+            }
+            if (t==i)
+            {
+                cout<<"user not found";
+            }
+            break;
+            
+        
+        default:
+            break;
+        }
+        
+    }
     
 }
