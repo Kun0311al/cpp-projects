73e05e20a8ae71d6ac239da6a415e6fe9d50e43b
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-13 14:53:16 +0530

    adding new items

--- hotel_billing.cpp
+++ hotel_billing.cpp
@@ -5,24 +5,32 @@ using namespace std;
 class hotel{
     private:
         int *price;
-        char *dine_in, *take_away;
+        char *dine_in, *take_away, *new_dish,*type_dish;
     public:
         hotel(){
+            new_dish = new char[20];
             price = new int[10];
             dine_in = new char[20];
             take_away = new char[20];
+            type_dish = new char[20];
         }
+        void add_items();
         void show_menu();
         void dine_in();
         void take_away();
 };
 
-void hotel::show_menu(){
-
+void hotel::add_items(){
+    cout<<"Enter Name of new dish: "; cin.getline(new_dish,20);
+    cout<<"Enter the class of dish: "; cin.getline(type_dish,20);
+    cout<<"Enter the price of dish: "; cin>>*price;
 }
+
+
 
 int main(){
     system("CLS");
+    hotel *h[100];
     int total_bill{},i,t,choice;
     char Dish[50];
 
@@ -30,16 +38,25 @@ int main(){
     while(1){
         cout<<"Welcome to Modern Pride\n";
         cout<<"\nHow Can I Help You Sir/ Maam?\n";
-        cout<<"1. Show Menu\n";
-        cout<<"2. Dine-In Order\n";
-        cout<<"3. Take Away Order\n";
-        cout<<"4. Complaint /Suggestions\n";
-        cout<<"5. Contact Us\n";
+        cout<<"1. Add new item\n";
+        cout<<"2. Show Menu\n";
+        cout<<"3. Dine-In Order\n";
+        cout<<"4. Take Away Order\n";
+        cout<<"5. Complaint /Suggestions\n";
+        cout<<"6. Contact Us\n";
         cin>>choice;
 
         switch(choice){
             case 1: 
-                show_menu();
+                h[i]= new hotel;
+                h[i] -> add_items();
+                break;
+            
+            case 2:
+                cout<<"Menu\n";
+                for(t=0;t<i;t++){
+                    h[t] -> show_menu();
+                }
                 break;
         }
     }
